0282fda1a3e10c1ba377d62e13d7366df8df558f
Connor Mulligan <connor26m@gmail.com>
2022-11-16 23:10:55 -0700

    added further functionality to the JS. need to finish score tracking

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -20,13 +20,13 @@ var timerCount = document.querySelector("#timer-count");
 // Start Screen
 var startScreen = document.querySelector(".start-screen");
 var startBtn = startScreen.querySelector("#start-btn");
+var displayNumQuizQuestions = startScreen.querySelector("#display-num-quiz-questions");
+var timeForQuiz = startScreen.querySelector("#time-for-quiz");
 
 // Quiz
 var quizScreens = document.querySelector(".quiz");
 
 var quizQuestion = quizScreens.querySelector("#quiz-question");
-quizQuestion.dataset.questionIndex = "0"
-var currentQuestionNumber = quizQuestion.dataset.questionIndex
 
 var quizOptionBox = quizScreens.querySelector(".question-options")
 var quizOption1 = quizScreens.querySelector("#option-1");
@@ -47,54 +47,6 @@ var tryAgainBtn = quizResultsScreen.querySelector("#try-again-btn");
 
 
 // Create necessary global variables
-var startTime = "25";
-timerCount.textContent = startTime;
-var timeRemaining = parseInt(timerCount.textContent);
-var userScore = 0;
-
-
-// Make view scores button take you to high-scores page
-viewScoresBtn.addEventListener("click", function() {
-    window.location.href="./high-scores.html";
-});
-
-// Create timer function
-function setTime() {
-    var timerInterval = setInterval(function() {
-        timeRemaining--;
-        timerCount.textContent = timeRemaining;
-
-        if (timeRemaining < 10) {
-            timerCount.setAttribute("style", "color: red;")
-            } 
-
-        if(timeRemaining === 0) {
-          // Stops execution of action at set interval
-        clearInterval(timerInterval);
-        // Resets value of timeRemaining
-        timeRemaining = parseInt(startTime);
-        //   Takes the user to the quiz results screen
-        quizScreens.setAttribute("style", "display: none;");
-        quizResultsScreen.setAttribute("style", "display: block;")
-        }
-      }, 1000);
-}
-
-// Create event listener for start button, hide start screen show quiz screen.
-startBtn.addEventListener("click", function(){
-    setTimeout(function() {
-        startScreen.style.display = "none";
-        quizScreens.style.display = "block";
-        viewScoresBtn.style.display = "none";
-        header.style.justifyContent = "center";
-    }, 500);
-
-    setTimeout(setTime(), 500); 
-    setTimeout(createQuizScreen(), 500);
-
-});
-
-
 var quizData = [
     {
         "question": "What are the 3 logical operators?",
@@ -121,33 +73,103 @@ var quizData = [
     }
 ]
 
-function createQuizScreen() {
-    quizQuestion.textContent = quizData[currentQuestionNumber].question;
+timeRemaining = 50;
+timerCount.textContent = timeRemaining;
 
-    quizOption1.textContent = quizData[currentQuestionNumber].answers[0];
-    quizOption2.textContent = quizData[currentQuestionNumber].answers[1];
-    quizOption3.textContent = quizData[currentQuestionNumber].answers[2];
-    quizOption4.textContent = quizData[currentQuestionNumber].answers[3];
+timeForQuiz.textContent = timeRemaining.toString();
+displayNumQuizQuestions.textContent = quizData.length.toString();
 
-    // var correctAnswer = quizData[currentQuestionNumber].answers[quizData[currentQuestionNumber].correctAnswerIndex];
-    var correctAnswer = quizData[currentQuestionNumber].correctAnswerIndex;
+var currentQuestionNumber = 0;
 
-    if (correctAnswer === 0) {
-        quizOption1.dataset.answerStatus = "correct";
-    } else if (correctAnswer === 1) {
-        quizOption2.dataset.answerStatus = "correct";
-    } else if (correctAnswer === 2) {
-        quizOption3.dataset.answerStatus = "correct";
-    } else if (correctAnswer === 3) {
-        quizOption4.dataset.answerStatus = "correct";
-    }
+var userScore = 0;
 
+var quizScreenDelay = 500;
+
+
+// Make view scores button take you to high-scores page
+viewScoresBtn.addEventListener("click", function() {
+    window.location.href="./high-scores.html";
+});
+
+// Create timer function
+function setTime() {
+    var timerInterval = setInterval(function() {
+        timeRemaining--;
+        timerCount.textContent = timeRemaining;
+
+        if (timeRemaining < 10) {
+            timerCount.style.color = "red";
+        } 
+
+        if(timeRemaining === 0 || currentQuestionNumber > quizData.length - 1) {
+          // Stops execution of action at set interval
+        clearInterval(timerInterval);
+        }
+      }, 1000);
 }
 
-// function checkUserSelection(event) {
-//     var element = event.target
+// Create event listener for start button, hide start screen show quiz screen.
+startBtn.addEventListener("click", function(){
+    setTimeout(function() {
+        startScreen.style.display = "none";
+        quizResultsScreen.style.display = "none";
+        quizScreens.style.display = "block";
+        viewScoresBtn.style.display = "none";
+        header.style.justifyContent = "center";
+    }, 500);
 
-// }
+    setTimeout(setTime(), quizScreenDelay); 
+    createQuizScreen();
+
+});
+
+
+function createQuizScreen() {
+
+
+// check if we have not exceeded the number of questions in our quizData array. if we have not, create
+// screen, otherwise, wait the delay time then go to the quiz results screen.
+    if (currentQuestionNumber <= quizData.length - 1) {
+
+    setTimeout(function(){
+        quizQuestion.textContent = quizData[currentQuestionNumber].question;
+
+        quizOption1.style.backgroundColor = "#fff"
+        quizOption2.style.backgroundColor = "#fff"
+        quizOption3.style.backgroundColor = "#fff"
+        quizOption4.style.backgroundColor = "#fff"
+
+        quizOption1.textContent = quizData[currentQuestionNumber].answers[0];
+        quizOption2.textContent = quizData[currentQuestionNumber].answers[1];
+        quizOption3.textContent = quizData[currentQuestionNumber].answers[2];
+        quizOption4.textContent = quizData[currentQuestionNumber].answers[3];
+    }, quizScreenDelay)
+        
+
+
+        var correctAnswer = quizData[currentQuestionNumber].correctAnswerIndex;
+
+        if (correctAnswer === 0) {
+            quizOption1.dataset.answerStatus = "correct";
+        } else if (correctAnswer === 1) {
+            quizOption2.dataset.answerStatus = "correct";
+        } else if (correctAnswer === 2) {
+            quizOption3.dataset.answerStatus = "correct";
+        } else if (correctAnswer === 3) {
+            quizOption4.dataset.answerStatus = "correct";
+        }
+
+        quizOptionBox.style.pointerEvents = "all";
+
+        quizScreenDelay = 2000;
+    } else {
+        setTimeout(function(){
+            quizScreens.style.display = "none";
+        quizResultsScreen.style.display = "block";
+        }, quizScreenDelay)
+    }
+}
+
 // Set event listener for the quiz options, needs to check whether answer is correct or incorrect.
 // set background of clicked element, show the appropriate icon in the box, and set header footer
 // and remove time if necessary
@@ -156,29 +178,61 @@ function createQuizScreen() {
 quizOptionBox.addEventListener("click", function(event){
     var element = event.target;
 
-    element.style.backgroundColor = "#fcd9c3"
+    quizOption1.style.backgroundColor = "#fff"
+    quizOption2.style.backgroundColor = "#fff"
+    quizOption3.style.backgroundColor = "#fff"
+    quizOption4.style.backgroundColor = "#fff"
 
+    // quizOptionBox.disabled = true
     if (element.dataset.answerStatus === "correct") {
-        
+        element.style.backgroundColor = "#fcd9c3"
+        quizOptionBox.style.backgroundColor = "#fff";
+        userScore += userScore + 10;
+        userSelectionResult.textContent = "Correct! "
+        var correctIcon = document.createElement("i");
+        correctIcon.classList.add("bi-check-circle");
+        userSelectionResult.append(correctIcon);
+        quizOptionBox.style.pointerEvents = "none";
+
+    } else {
+        element.style.backgroundColor = "#fcd9c3"
+        quizOptionBox.style.backgroundColor = "#fff";
+        timeRemaining -= 10;
+        timerCount.style.color = "red";
+        setTimeout(function() {
+            timerCount.style.color = "black";
+        }, 500)
+        userSelectionResult.textContent = "Incorrect "
+        var incorrectIcon = document.createElement("i");
+        incorrectIcon.classList.add("bi-x-circle");
+        userSelectionResult.append(incorrectIcon);
+        quizOptionBox.setAttribute("style", "pointer-events: none;")
     }
+    console.log(currentQuestionNumber);
+    currentQuestionNumber += 1
 
-    console.log(element.dataset.answerStatus);
+    createQuizScreen();
+
+});
 
 
-})
+function processScore(){
 
-
-
+}
 
 
 // Create event listener for try again button
 tryAgainBtn.addEventListener("click", function(){
     setTimeout(function() {
-        timerCount.textContent = startTime;
+        timeRemaining = 50;
+        timerCount.textContent = timeRemaining;
         timerCount.style.color = "";
+        quizScreenDelay = 500;
         quizResultsScreen.style.display = "none";
         startScreen.style.display = "block";
         viewScoresBtn.style.display = "";
         header.style.justifyContent = "";
+        
+        currentQuestionNumber = 0;
     }, 500);
 });
